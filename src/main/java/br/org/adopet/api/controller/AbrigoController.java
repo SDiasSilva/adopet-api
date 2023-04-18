a3f5a15456bd627f9695dbe212a1ae3165fc8531
juniorsi <juniorsi@SAO3N001941.accounts.intern>
2023-04-18 15:45:46 -0300

    Requisição (GET) para exibir abrigo por ID

--- src/main/java/br/org/adopet/api/controller/AbrigoController.java
+++ src/main/java/br/org/adopet/api/controller/AbrigoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +38,13 @@ public class AbrigoController {
 		}
 		return ResponseEntity.ok(abrigos);
 	}
+	
+	@GetMapping({"/{id}"})
+	public ResponseEntity<AbrigoListagemDTO> get(@PathVariable Long id){
+		Abrigo abrigo = abrigoRepository.getReferenceById(id);
+		return ResponseEntity.ok(new AbrigoListagemDTO(abrigo));
+	}
+	
 	
 	@PostMapping
 	@Transactional
