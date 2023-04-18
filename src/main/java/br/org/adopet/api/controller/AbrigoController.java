4b6045236ccfaa4b65d919c7c9f482fffb5b5a1c
juniorsi <juniorsi@SAO3N001941.accounts.intern>
2023-04-18 15:38:37 -0300

    Requisição (GET) para exibir todos os abrigos

--- src/main/java/br/org/adopet/api/controller/AbrigoController.java
+++ src/main/java/br/org/adopet/api/controller/AbrigoController.java
@@ -1,7 +1,10 @@
 package br.org.adopet.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import br.org.adopet.api.domain.model.Abrigo;
 import br.org.adopet.api.domain.model.Cidade;
 import br.org.adopet.api.domain.repository.AbrigoRepository;
 import br.org.adopet.api.domain.repository.CidadeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import br.org.adopet.api.domain.dto.AbrigoCadastroDTO;
@@ -24,6 +28,15 @@ public class AbrigoController {
 	
 	@Autowired
 	CidadeRepository cidadeRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<AbrigoListagemDTO>> get(){
+		List<AbrigoListagemDTO> abrigos = abrigoRepository.findAll().stream().map(AbrigoListagemDTO::new).toList();
+		if(abrigos.size() == 0) {
+			throw new EntityNotFoundException();
+		}
+		return ResponseEntity.ok(abrigos);
+	}
 	
 	@PostMapping
 	@Transactional
