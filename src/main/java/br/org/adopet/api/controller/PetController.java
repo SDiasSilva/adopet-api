ab2edabbada705c44911db904c956f37cade1c80
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-04-19 13:41:34 -0300

    Requisição (GET) para exibir pet pelo ID

--- src/main/java/br/org/adopet/api/controller/PetController.java
+++ src/main/java/br/org/adopet/api/controller/PetController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,7 +44,11 @@ public class PetController {
 		return ResponseEntity.ok(pets);
 	}
 	
-	
+	@GetMapping("/{id}")
+	public ResponseEntity<PetListagemDTO> get(@PathVariable Long id){
+		Pet pet = petRepository.getReferenceById(id);
+		return ResponseEntity.ok(new PetListagemDTO(pet));
+	}
 	
 	@PostMapping
 	@Transactional
