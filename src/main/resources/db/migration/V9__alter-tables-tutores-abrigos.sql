baea2c0b9936bfb5b62200435eea2a572d12a510
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-04-22 16:19:31 -0300

    Correção na migration V9 que estava com as fk com nome repetido.

--- src/main/resources/db/migration/V9__alter-tables-tutores-abrigos.sql
+++ src/main/resources/db/migration/V9__alter-tables-tutores-abrigos.sql
@@ -1,10 +1,10 @@
 ALTER TABLE tutores
 ADD COLUMN usuario_id INT,
-ADD CONSTRAINT fk_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
+ADD CONSTRAINT fk_tutor_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
 DROP COLUMN email,
 DROP COLUMN senha;
 
 ALTER TABLE abrigos
 ADD COLUMN usuario_id INT,
-ADD CONSTRAINT fk_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
+ADD CONSTRAINT fk_abrigo_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
 DROP COLUMN email;
