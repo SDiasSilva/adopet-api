4467a2c12a02ab0f86f60f682f0b7b9df9bee0f3
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-04-23 17:30:59 -0300

    Requisição (GET) para exibir todos as adoções de maneira paginada

--- src/main/java/br/org/adopet/api/controller/AdocaoController.java
+++ src/main/java/br/org/adopet/api/controller/AdocaoController.java
@@ -1,8 +1,13 @@
 package br.org.adopet.api.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,20 +23,27 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("adocao")
-public class AdocaoController extends BaseController{
-	
+public class AdocaoController extends BaseController {
+
+	@GetMapping
+	public ResponseEntity<Page<AdocaoDetalhamentoDTO>> getListarAdocoes(
+			@PageableDefault(size = 9, sort = { "data" }, direction = Direction.DESC) Pageable paginacao) {
+		Page<AdocaoDetalhamentoDTO> paginaAdocoes = super.adocaoRepository().findAll(paginacao).map(AdocaoDetalhamentoDTO::new);
+		return ResponseEntity.ok(paginaAdocoes);
+	}
+
 	@PostMapping
 	@Transactional
-	public ResponseEntity<AdocaoDetalhamentoDTO> postCriarAdocao(@RequestBody @Valid AdocaoCadastro dadosAdocao){
+	public ResponseEntity<AdocaoDetalhamentoDTO> postCriarAdocao(@RequestBody @Valid AdocaoCadastro dadosAdocao) {
 		Pet pet = super.buscarEntidade(dadosAdocao.petId(), super.petRepository(), "pet");
 		Tutor tutor = super.buscarEntidade(dadosAdocao.tutorId(), super.tutorRepository(), "tutor");
 		Adocao adocao = super.adocaoRepository().save(new Adocao(pet, tutor));
 		return ResponseEntity.ok(new AdocaoDetalhamentoDTO(adocao));
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@Transactional
-	public ResponseEntity<MensagemDTO> deleteCancelarAdocao(@PathVariable Long id){
+	public ResponseEntity<MensagemDTO> deleteCancelarAdocao(@PathVariable Long id) {
 		Adocao adocao = super.buscarEntidade(id, super.adocaoRepository(), "adocao");
 		adocao.cancelar();
 		super.adocaoRepository().delete(adocao);
